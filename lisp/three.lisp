b71269b68eea839cab78b3972e7b594d788ea023
t-cool <padds1st@gmail.com>
2018-11-30 20:36:15 +0900

    Update three.lisp

--- lisp/three.lisp
+++ lisp/three.lisp
@@ -2,10 +2,13 @@
 ;; Cmd-E: send the code to REPL and evaluate it.
 ;; Please reload the Window when you try another example.
 
-;; color
-(setf #j:threejs:material:color:r 1)
-(setf #j:threejs:material:color:b 0)
-(setf #j:threejs:material:color:g 0)
+;; change color
+(defun change-color(r g b)
+  (setf #j:threejs:material:color:r r)
+  (setf #j:threejs:material:color:g g)
+  (setf #j:threejs:material:color:b b))
+ 
+(change-color 1 1 0)
 
 ;; wireframe off
 (setf #j:threejs:material:wireframe 0)
